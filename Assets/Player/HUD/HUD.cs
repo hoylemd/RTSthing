b03c40385a69e7de4edb33535a9dcfb113b4a115
Michael D. Hoyle <hoylemd@gmail.com>
2015-04-24 06:48:02 -0400

    draw orders bar

--- Assets/Player/HUD/HUD.cs
+++ Assets/Player/HUD/HUD.cs
@@ -5,14 +5,29 @@ public class HUD : MonoBehaviour {
 
 	public GUISkin resourceSkin, ordersSkin;
 	private const int ORDERS_BAR_WIDTH = 150, RESOURCE_BAR_HEIGHT = 40;
+	private Player player;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		player = transform.root.GetComponent<Player>();
 	}
-	
+
+	void DrawOrdersBar() {
+		GUI.skin = ordersSkin;
+		GUI.BeginGroup(new Rect(Screen.width-ORDERS_BAR_WIDTH, RESOURCE_BAR_HEIGHT, ORDERS_BAR_WIDTH, Screen.width - RESOURCE_BAR_HEIGHT));
+		GUI.Box (new Rect(0, 0, ORDERS_BAR_WIDTH, Screen.height - RESOURCE_BAR_HEIGHT), "");
+		GUI.EndGroup();
+	}
+
+	void DrawResourceBar() {
+
+	}
+
 	// Update is called once per frame
-	void Update () {
-	
+	void OnGUI() {
+		if (player && player.human) {
+			DrawOrdersBar();
+			DrawResourceBar();
+		}
 	}
 }
