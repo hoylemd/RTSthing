53c8a6d85cfd9cef1b2ae93a29d08f83d368b818
Michael D. Hoyle <hoylemd@gmail.com>
2015-04-24 09:19:23 -0400

    start WorldObject stuff

---
+++ Assets/WorldObject/WorldObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldObject : MonoBehaviour {
+	public string objectName;
+	public Texture2D buildImage;
+	public int cost, sellValue, hitPoints, maxHitPoints;
+
+	protected Player player;
+	protected string[] actions = {};
+	protected bool currentlySelected = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
