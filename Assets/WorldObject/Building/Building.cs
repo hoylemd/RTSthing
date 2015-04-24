f9e2ffc785a9f982f02fbdc7011554d0d60ccdf5
Michael D. Hoyle <hoylemd@gmail.com>
2015-04-24 12:38:41 -0400

    add Building

---
+++ Assets/WorldObject/Building/Building.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : WorldObject {
+	protected virtual new void Awake() {
+    base.Awake();
+  }
+
+	// Use this for initialization
+	protected virtual new void Start() {
+    base.Start();
+	}
+
+	// Update is called once per frame
+	protected virtual new void Update() {
+    base.Update();
+	}
+
+	protected virtual new void OnGUI() {
+    base.OnGUI();
+  }
+}
