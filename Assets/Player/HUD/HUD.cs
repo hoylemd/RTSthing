734a11fd270ef21528f7c711f5e50a349743f66a
Michael D. Hoyle <hoylemd@gmail.com>
2015-04-24 06:40:58 -0400

    added HUD objects

---
+++ Assets/Player/HUD/HUD.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+	public GUISkin resourceSkin, ordersSkin;
+	private const int ORDERS_BAR_WIDTH = 150, RESOURCE_BAR_HEIGHT = 40;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
