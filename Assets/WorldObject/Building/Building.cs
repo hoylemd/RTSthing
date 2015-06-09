245d41bb94404228f0f4a77ec23acd679207f2da
Michael D. Hoyle <hoylemd@gmail.com>
2015-06-09 13:29:04 -0400

    Added tank

--- Assets/WorldObject/Building/Building.cs
+++ Assets/WorldObject/Building/Building.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class Building : WorldObject {
 	protected virtual new void Awake() {
-    base.Awake();
-  }
+		base.Awake();
+	}
 
 	// Use this for initialization
 	protected virtual new void Start() {
-    base.Start();
+		base.Start();
 	}
 
 	// Update is called once per frame
 	protected virtual new void Update() {
-    base.Update();
+		base.Update();
 	}
 
 	protected virtual new void OnGUI() {
-    base.OnGUI();
-  }
+		base.OnGUI();
+	}
 }
