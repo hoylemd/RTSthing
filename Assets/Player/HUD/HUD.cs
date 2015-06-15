98da1c0f65dc8fa8bb3ee319f09e165c83fd81b0
Michael D. Hoyle <hoylemd@gmail.com>
2015-06-14 21:07:21 -0400

    mouse cursor controls

--- Assets/Player/HUD/HUD.cs
+++ Assets/Player/HUD/HUD.cs
@@ -12,12 +12,17 @@ public class HUD : MonoBehaviour {
 	public Texture2D activeCursor, selectCursor,
 		   leftCursor, upCursor, rightCursor, downCursor;
 	public Texture2D[] attackCursors, harvestCursors, moveCursors;
+	public GUISkin mouseCursorSkin;
+	private CursorState activeCursorState;
+	private int currentFrame = 0;
 
 	// Use this for initialization
 	void Start() {
 		player = transform.root.GetComponent<Player>();
 
 		ResourceManager.StoreSelectBoxItems(selectBoxSkin);
+
+		SetCursorState(CursorState.Select);
 	}
 
 	void DrawOrdersBar() {
@@ -49,11 +54,77 @@ public class HUD : MonoBehaviour {
 		GUI.EndGroup();
 	}
 
+	void UpdateCursorAnimation() {
+		// animate 1 frame / second
+		if(activeCursorState == CursorState.Move) {
+			currentFrame = (int)Time.time % moveCursors.Length;
+			activeCursor = moveCursors[currentFrame];
+		} else if(activeCursorState == CursorState.Attack) {
+			currentFrame = (int)Time.time % attackCursors.Length;
+			activeCursor = attackCursors[currentFrame];
+		} else if(activeCursorState == CursorState.Harvest) {
+			currentFrame = (int)Time.time % harvestCursors.Length;
+			activeCursor = harvestCursors[currentFrame];
+		}
+	}
+	private Rect GetCursorDrawPosition() {
+	    //set base position for custom cursor image
+	    float leftPos = Input.mousePosition.x;
+	    float topPos = Screen.height - Input.mousePosition.y;
+	    //adjust position base on the type of cursor being shown
+	    if (activeCursorState == CursorState.PanRight) {
+			leftPos = Screen.width - activeCursor.width;
+	    } else if (activeCursorState == CursorState.PanDown) {
+			topPos = Screen.height - activeCursor.height;
+	    } else if (activeCursorState == CursorState.Move ||
+				activeCursorState == CursorState.Select ||
+				activeCursorState == CursorState.Harvest) {
+	        topPos -= activeCursor.height / 2;
+	        leftPos -= activeCursor.width / 2;
+	    }
+	    return new Rect(leftPos, topPos, activeCursor.width, activeCursor.height);
+	}
+
+	void DrawMouseCursor() {
+		if (!MouseInBounds()) {
+			Cursor.visible = false;
+		} else {
+			Cursor.visible = true;
+			GUI.skin = mouseCursorSkin;
+			GUI.BeginGroup(new Rect(0,0,Screen.width,Screen.height));
+			UpdateCursorAnimation();
+			Rect cursorPosition = GetCursorDrawPosition();
+			GUI.Label(cursorPosition, activeCursor);
+			GUI.EndGroup();
+		}
+	}
+
+	public void SetCursorState(CursorState newState) {
+	    activeCursorState = newState;
+	    switch(newState) {
+		    case CursorState.Select:
+		        activeCursor = selectCursor;
+		        break;
+		    case CursorState.Attack:
+		        currentFrame = (int)Time.time % attackCursors.Length;
+		        activeCursor = attackCursors[currentFrame];
+		        break;
+		    case CursorState.Harvest:
+		        currentFrame = (int)Time.time % harvestCursors.Length;
+		        activeCursor = harvestCursors[currentFrame];
+		        break;
+		    case CursorState.Move:
+		        currentFrame = (int)Time.time % moveCursors.Length;
+		        activeCursor = moveCursors[currentFrame];
+				break;
+		}
+	}
 	// Update is called once per frame
 	void OnGUI() {
 		if (player && player.human) {
 			DrawOrdersBar();
 			DrawResourceBar();
+			DrawMouseCursor();
 		}
 	}
 
@@ -66,7 +137,7 @@ public class HUD : MonoBehaviour {
 	}
 
 	public Rect GetPlayingArea() {
-		return new Rect(0, RESOURCE_BAR_HEIGHT, Screen.width - ORDERS_BAR_WIDTH, 
+		return new Rect(0, RESOURCE_BAR_HEIGHT, Screen.width - ORDERS_BAR_WIDTH,
 		                Screen.height - RESOURCE_BAR_HEIGHT);
 	}
 }
