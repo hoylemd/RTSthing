074a8f7f26203b70c69ccc7d216258dfe6a3d6f2
Michael D. Hoyle <hoylemd@gmail.com>
2015-06-20 22:05:59 -0400

    add move starting

--- Assets/WorldObject/Unit/Unit.cs
+++ Assets/WorldObject/Unit/Unit.cs
@@ -4,29 +4,55 @@ using RTS;
 
 public class Unit : WorldObject {
 
+    public bool moving, rotating;
+
+    private Vector3 destination;
+    private Quaternion targetRotation;
+
     /*** Game Engine methods, all can be overridden by subclass ***/
 
     protected override void Awake() {
-		base.Awake();
-	}
+        base.Awake();
+    }
 
     protected override void Start () {
-		base.Start();
-	}
+        base.Start();
+    }
 
     protected override void Update () {
-		base.Update();
-	}
+        base.Update();
+    }
 
-	public override void SetHoverState(GameObject hoverObject) {
-		base.SetHoverState(hoverObject);
+    public override void SetHoverState(GameObject hoverObject) {
+        base.SetHoverState(hoverObject);
 
-		if (player && player.human && currentlySelected) {
-			if(hoverObject.name == "Ground") player.hud.SetCursorState(CursorState.Move);
-		}
-	}
+        if (player && player.human && currentlySelected) {
+            if (hoverObject.name == "Ground") player.hud.SetCursorState(CursorState.Move);
+        }
+    }
 
     protected override void OnGUI() {
-		base.OnGUI();
-	}
+        base.OnGUI();
+    }
+
+    public override void MouseClick(GameObject hitObject, Vector3 hitPoint, Player controller) {
+        base.MouseClick(hitObject, hitPoint, controller);
+
+        if (player && player.human && currentlySelected) {
+            if (hitObject.name == "Ground" && hitPoint != ResourceManager.InvalidPosition) {
+                StartMove(new Vector3(
+                    hitPoint.x,
+                    hitPoint.y + GetComponent<Transform>().position.y,
+                    hitPoint.z));
+            }
+        }
+    }
+
+    private void StartMove(Vector3 dest) {
+        destination = dest;
+        targetRotation = Quaternion.LookRotation(
+            destination - GetComponent<Transform>().position);
+        rotating = true;
+        moving = false;
+    }
 }
--- Assets/WorldObject/WorldObject.cs
+++ Assets/WorldObject/WorldObject.cs
@@ -3,100 +3,100 @@ using System.Collections;
 using RTS;
 
 public class WorldObject : MonoBehaviour {
-	public string objectName;
-	public Texture2D buildImage;
-	public int cost, sellValue, hitPoints, maxHitPoints;
+    public string objectName;
+    public Texture2D buildImage;
+    public int cost, sellValue, hitPoints, maxHitPoints;
 
-	protected Player player;
-	protected string[] actions = {};
-	protected bool currentlySelected = false;
+    protected Player player;
+    protected string[] actions = {};
+    protected bool currentlySelected = false;
 
-	protected Bounds selectionBounds;
-	protected Rect playingArea = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+    protected Bounds selectionBounds;
+    protected Rect playingArea = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
 
-	protected virtual void Awake() {
-		selectionBounds = ResourceManager.InvalidBounds;
-		CalculateBounds();
-	}
+    protected virtual void Awake() {
+        selectionBounds = ResourceManager.InvalidBounds;
+        CalculateBounds();
+    }
 
-	// Use this for initialization
-	protected virtual void Start() {
-		player = GetComponentInParent<Player>();
-	}
+    // Use this for initialization
+    protected virtual void Start() {
+        player = GetComponentInParent<Player>();
+    }
 
-	// Update is called once per frame
-	protected virtual void Update() {
+    // Update is called once per frame
+    protected virtual void Update() {
 
-	}
+    }
 
-	private void ChangeSelection(WorldObject worldObject, Player controller) {
-		SetSelection(false, playingArea);
-		if (controller.selectedObject) {
-			controller.selectedObject.SetSelection(false, playingArea);
-		}
-		controller.selectedObject = worldObject;
-		worldObject.SetSelection(true, controller.hud.GetPlayingArea());
-	}
+    private void ChangeSelection(WorldObject worldObject, Player controller) {
+        SetSelection(false, playingArea);
+        if (controller.selectedObject) {
+            controller.selectedObject.SetSelection(false, playingArea);
+        }
+        controller.selectedObject = worldObject;
+        worldObject.SetSelection(true, controller.hud.GetPlayingArea());
+    }
 
-	public virtual void MouseClick(GameObject hitObject, Vector3 hitPoint,
-			Player controller) {
-		// only handle click if selected
-		if (currentlySelected && hitObject && hitObject.name != "Ground") {
-			WorldObject worldObject =
-				hitObject.transform.parent.GetComponent<WorldObject>();
-			// clicked on another world object
-			if (worldObject) ChangeSelection(worldObject, controller);
-		}
-	}
+    public virtual void MouseClick(GameObject hitObject, Vector3 hitPoint,
+            Player controller) {
+        // only handle click if selected
+        if (currentlySelected && hitObject && hitObject.name != "Ground") {
+            WorldObject worldObject =
+                hitObject.transform.parent.GetComponent<WorldObject>();
+            // clicked on another world object
+            if (worldObject) ChangeSelection(worldObject, controller);
+        }
+    }
 
-	public void CalculateBounds() {
-		selectionBounds = new Bounds(transform.position, Vector3.zero);
-		foreach(Renderer r in GetComponentsInChildren<Renderer>()) {
-			selectionBounds.Encapsulate(r.bounds);
-		}
-	}
+    public void CalculateBounds() {
+        selectionBounds = new Bounds(transform.position, Vector3.zero);
+        foreach(Renderer r in GetComponentsInChildren<Renderer>()) {
+            selectionBounds.Encapsulate(r.bounds);
+        }
+    }
 
-	protected virtual void DrawSelectionBox(Rect selectBox) {
-		GUI.Box(selectBox, "");
-	}
+    protected virtual void DrawSelectionBox(Rect selectBox) {
+        GUI.Box(selectBox, "");
+    }
 
-	private void DrawSelection() {
-		GUI.skin = ResourceManager.SelectBoxSkin;
-		Rect selectionBox = WorkManager.CalculateSelectionBox(selectionBounds, playingArea);
-		// Draw box
-		GUI.BeginGroup(playingArea);
-		DrawSelectionBox(selectionBox);
-		GUI.EndGroup();
-	}
+    private void DrawSelection() {
+        GUI.skin = ResourceManager.SelectBoxSkin;
+        Rect selectionBox = WorkManager.CalculateSelectionBox(selectionBounds, playingArea);
+        // Draw box
+        GUI.BeginGroup(playingArea);
+        DrawSelectionBox(selectionBox);
+        GUI.EndGroup();
+    }
 
-	protected virtual void OnGUI() {
-		if (currentlySelected) {
-			DrawSelection();
-		}
-	}
+    protected virtual void OnGUI() {
+        if (currentlySelected) {
+            DrawSelection();
+        }
+    }
 
-	public void SetSelection(bool selected, Rect playingArea) {
-		currentlySelected = selected;
-		if (selected) {
-			this.playingArea = playingArea;
-		}
-	}
+    public void SetSelection(bool selected, Rect playingArea) {
+        currentlySelected = selected;
+        if (selected) {
+            this.playingArea = playingArea;
+        }
+    }
 
-	public virtual void SetHoverState(GameObject hoverObject) {
-		// only handle if player is human, and this object is selected
-		if (player && player.human && currentlySelected) {
-			if (hoverObject.name != "Ground") {
-				player.hud.SetCursorState(CursorState.Select);
+    public virtual void SetHoverState(GameObject hoverObject) {
+        // only handle if player is human, and this object is selected
+        if (player && player.human && currentlySelected) {
+            if (hoverObject.name != "Ground") {
+                player.hud.SetCursorState(CursorState.Select);
 
-			}
-		}
-	}
+            }
+        }
+    }
 
-	public string[] GetActions() {
-		return actions;
-	}
+    public string[] GetActions() {
+        return actions;
+    }
 
-	public virtual void PerformAction(string action) {
-		// children will hande
-	}
+    public virtual void PerformAction(string action) {
+        // children will hande
+    }
 }
